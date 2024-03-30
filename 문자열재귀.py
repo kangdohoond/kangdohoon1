6151699cc88902d5540de2546df1741b1f33822e
kangdohoon1 <kiitii1113@naver.com>
2024-03-30 19:32:23 +0900

    데이터구조 재귀함수 문자열 도달과제 풀어보았습니다. 풀어보니 하노이탑과 비슷한 알고리즘이었습니다.

---
+++ 문자열재귀.py
@@ -0,0 +1,25 @@
+def find(curr, tail, size):
+    if curr == tail:
+        return 0 # 0 뜨면 조합 성공
+    elif len(curr) == size:
+        return 1
+
+    find(curr + 'A', tail, size) # 이진트리 느낌
+    if find(curr + 'A', tail, size) == 0:
+        return 0
+
+    curr += 'B' # 하노이탑 알고리즘이랑 비슷
+    curr = curr[::-1] # 역순으로 바꿔주는 코드
+    find(curr, tail, size)   # 이진트리 느낌
+
+    return find(curr, tail, size)
+
+arr = input("A와 B로 이루어진 문자열 입력: ")
+end = input("조합할 AB: ")
+
+find(arr, end, len(end)) # current,tail,tail길이
+
+if find(arr, end, len(end)) == 0:
+    print("1")
+else:
+    print("0")
